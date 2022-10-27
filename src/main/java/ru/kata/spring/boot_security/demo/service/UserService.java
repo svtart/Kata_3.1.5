045fb3a8d382001fb60b8b3dab2d41c73f85cd1b
svtart <svetoy_a@mail.ru>
2022-10-27 21:28:40 +0300

    First class implementation

---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
@@ -0,0 +1,16 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import ru.kata.spring.boot_security.demo.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    public List<User> index();
+
+    public User show(int id);
+    public void save(User user);
+
+    public void update(int id, User user);
+
+    public void delete(int id);
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -0,0 +1,47 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.dao.UserDAO;
+import ru.kata.spring.boot_security.demo.model.User;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDAO userDAO;
+
+
+    @Override
+    @Transactional
+    public List<User> index() {
+        return userDAO.index();
+    }
+
+    @Override
+    @Transactional
+    public User show(int id) {
+        return userDAO.show(id);
+    }
+
+    @Override
+    @Transactional
+    public void save(User user) {
+        userDAO.save(user);
+    }
+
+
+    @Override
+    @Transactional
+    public void update(int id, User user) {
+        userDAO.update(id, user);
+    }
+    @Override
+    @Transactional
+    public void delete(int id) {
+        userDAO.delete(id);
+    }
+}
