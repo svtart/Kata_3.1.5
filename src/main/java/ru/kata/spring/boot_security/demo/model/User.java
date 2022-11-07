6615aec873c9c799c28285d4e013ebb14fb48e2b
svtart <svetoy_a@mail.ru>
2022-11-07 15:19:06 +0300

    BCryptPassword 1.0

--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Component;
 import javax.persistence.*;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 
